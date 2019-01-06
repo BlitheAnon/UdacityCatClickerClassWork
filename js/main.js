11e9ba63f429ba05ab775bf5dd35831aa3945b6e
blitheanon <blitheanon@qq.com>
2019-01-06 09:50:18 +0800

    Feature: index.html新增一个猫，添加名字，显示点击数

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 window.onload=function() {
-  var count=0;
-  var countShow=document.getElementById('clickedCount');
-  var picElement=document.getElementById('catPic');
-  picElement.addEventListener('click',function() {
-    count++;
-    countShow.innerText=count;
+  var count1=0;
+  var countShow1=document.getElementById('clickedCount1');
+  var picElement1=document.getElementById('catPic1');
+  picElement1.addEventListener('click',function() {
+    count1++;
+    countShow1.innerText=count1;
+  },false);
+
+
+  var count2=0;
+  var countShow2=document.getElementById('clickedCount2');
+  var picElement2=document.getElementById('catPic2');
+  picElement2.addEventListener('click',function() {
+    count2++;
+    countShow2.innerText=count2;
   },false);
 };

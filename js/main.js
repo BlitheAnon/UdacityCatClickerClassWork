255a5445914c012c515a8dabf1f95ee40818aca8
blitheanon <blitheanon@qq.com>
2019-01-06 15:59:55 +0800

    Feature: 新增猫列表，点击猫列表显示对应图片，每个图片有独自的点击数

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
+const catArr=['cat1','cat2','cat3','cat4','cat5','cat6'];
+let countArr=[0,0,0,0,0,0];
+let nowClickIndex=0;
+
 window.onload=function() {
-  var count1=0;
-  var countShow1=document.getElementById('clickedCount1');
-  var picElement1=document.getElementById('catPic1');
-  picElement1.addEventListener('click',function() {
-    count1++;
-    countShow1.innerText=count1;
-  },false);
 
+  let _listHtml='';
 
-  var count2=0;
-  var countShow2=document.getElementById('clickedCount2');
-  var picElement2=document.getElementById('catPic2');
-  picElement2.addEventListener('click',function() {
-    count2++;
-    countShow2.innerText=count2;
-  },false);
+  for (cat of catArr) {
+    _listHtml+=`<li>${cat}</li>`;
+  }
+
+  document.getElementById('catList').innerHTML=_listHtml;
+
+  //列表对象
+  let list=document.getElementsByTagName('li');
+  for(let i=0;i<list.length;i++){
+    let catName=list[i].innerText;
+    //监听列表点击
+    list[i].addEventListener('click',(function(catNameCopy) {
+      return function() {
+        console.log(countArr);
+        //替换猫图，猫名，点击数
+        document.getElementById('catName').innerText=catNameCopy;
+        document.getElementById('catPic').src=`images/${catNameCopy}.jpg`;
+        document.getElementById('clickedCount').innerText=countArr[i];
+        nowClickIndex=i;
+      };
+    })(catName));
+  };
+
+  //监听图片点击
+  let catPicElement=document.getElementById('catPic');
+  catPicElement.addEventListener('click',function() {
+    countArr[nowClickIndex]++;
+    console.log(countArr);
+    document.getElementById('clickedCount').innerText=countArr[nowClickIndex];
+  });
 };

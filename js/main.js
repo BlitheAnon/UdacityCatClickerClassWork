1641abced15277223db1dd9f7fa678aeb9578583
blitheanon <blitheanon@qq.com>
2019-01-07 09:58:54 +0800

    Refactor: 重构main.js为MVO模型

--- js/main.js
+++ js/main.js
@@ -1,39 +1,64 @@
-const catArr=['cat1','cat2','cat3','cat4','cat5','cat6'];
-let countArr=[0,0,0,0,0,0];
-let nowClickIndex=0;
+window.onload = function() {
+  //20190107修改为MVO(model view octopus)模型
 
-window.onload=function() {
-
-  let _listHtml='';
-
-  for (cat of catArr) {
-    _listHtml+=`<li>${cat}</li>`;
+  //模型层
+  let model = {
+    catArr: ['cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6'],
+    countArr: [0, 0, 0, 0, 0, 0],
+    nowClickIndex: 0
   }
 
-  document.getElementById('catList').innerHTML=_listHtml;
+  //视图层
+  //index.html
 
-  //列表对象
-  let list=document.getElementsByTagName('li');
-  for(let i=0;i<list.length;i++){
-    let catName=list[i].innerText;
-    //监听列表点击
-    list[i].addEventListener('click',(function(catNameCopy) {
-      return function() {
-        console.log(countArr);
-        //替换猫图，猫名，点击数
-        document.getElementById('catName').innerText=catNameCopy;
-        document.getElementById('catPic').src=`images/${catNameCopy}.jpg`;
-        document.getElementById('clickedCount').innerText=countArr[i];
-        nowClickIndex=i;
-      };
-    })(catName));
+  //连接层
+  let octopus = {
+    init: function() {
+      //生成猫名列表
+      let _listHtml = '';
+      for (cat of model.catArr) {
+        _listHtml += `<li>${cat}</li>`;
+      }
+      document.getElementById('catList').innerHTML = _listHtml;
+
+      //添加第一张猫图，添加默认点击数和猫名
+      let _picHtml = '';
+      _picHtml += '<img class="catPhotoStyle" id="catPic" src="images/cat1.jpg">';
+      document.getElementById('imgPart').innerHTML = _picHtml;
+      document.getElementById('catName').innerText = model.catArr[0];
+      document.getElementById('clickedCount').innerText = model.countArr[0];
+    },
+
+    listClickListener: function() {
+      //列表点击监听
+      let list = document.getElementsByTagName('li');
+      for (let i = 0; i < list.length; i++) {
+        let catName = list[i].innerText;
+        //监听列表点击
+        list[i].addEventListener('click', (function(catNameCopy) {
+          return function() {
+            //替换猫图，猫名，点击数
+            document.getElementById('catName').innerText = catNameCopy;
+            document.getElementById('catPic').src = `images/${catNameCopy}.jpg`;
+            document.getElementById('clickedCount').innerText = model.countArr[i];
+            model.nowClickIndex = i;
+          };
+        })(catName));
+      }
+    },
+
+    imageClickListener: function() {
+      //图片点击监听
+      let catPicElement = document.getElementById('catPic');
+      catPicElement.addEventListener('click', function() {
+        model.countArr[model.nowClickIndex]++;
+        document.getElementById('clickedCount').innerText = model.countArr[model.nowClickIndex];
+      });
+    }
   };
 
-  //监听图片点击
-  let catPicElement=document.getElementById('catPic');
-  catPicElement.addEventListener('click',function() {
-    countArr[nowClickIndex]++;
-    console.log(countArr);
-    document.getElementById('clickedCount').innerText=countArr[nowClickIndex];
-  });
+  octopus.init();
+  octopus.listClickListener();
+  octopus.imageClickListener();
+
 };

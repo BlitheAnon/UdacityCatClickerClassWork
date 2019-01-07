2ed75c00e23683b83cc68dd23b2ca55a1b5c530b
blitheanon <blitheanon@qq.com>
2019-01-07 11:24:41 +0800

    Feature: main.js新增视图层，修改octopus章鱼层

--- js/main.js
+++ js/main.js
@@ -9,26 +9,20 @@ window.onload = function() {
   }
 
   //视图层
-  //index.html
-
-  //连接层
-  let octopus = {
+  let catListView = {
     init: function() {
       //生成猫名列表
       let _listHtml = '';
-      for (cat of model.catArr) {
+      //通过octopus模型取得猫列表
+      let catArr = octopus.getCatArr();
+      let countArr = octopus.getCountArr();
+      for (cat of catArr) {
         _listHtml += `<li>${cat}</li>`;
       }
       document.getElementById('catList').innerHTML = _listHtml;
 
-      //添加第一张猫图，添加默认点击数和猫名
-      let _picHtml = '';
-      _picHtml += '<img class="catPhotoStyle" id="catPic" src="images/cat1.jpg">';
-      document.getElementById('imgPart').innerHTML = _picHtml;
-      document.getElementById('catName').innerText = model.catArr[0];
-      document.getElementById('clickedCount').innerText = model.countArr[0];
+      this.listClickListener();
     },
-
     listClickListener: function() {
       //列表点击监听
       let list = document.getElementsByTagName('li');
@@ -40,25 +34,63 @@ window.onload = function() {
             //替换猫图，猫名，点击数
             document.getElementById('catName').innerText = catNameCopy;
             document.getElementById('catPic').src = `images/${catNameCopy}.jpg`;
-            document.getElementById('clickedCount').innerText = model.countArr[i];
-            model.nowClickIndex = i;
+            document.getElementById('clickedCount').innerText = octopus.getCountArr()[i];
+            octopus.setNowClickIndex(i);
           };
         })(catName));
       }
+    }
+  };
+
+  let catView = {
+    init: function() {
+      //添加第一张猫图，添加默认点击数和猫名
+      let _picHtml = '';
+      _picHtml += '<img class="catPhotoStyle" id="catPic" src="images/cat1.jpg">';
+      document.getElementById('imgPart').innerHTML = _picHtml;
+      document.getElementById('catName').innerText = octopus.getCatArr()[0];
+      document.getElementById('clickedCount').innerText = octopus.getCountArr()[0];
+
+      this.imageClickListener();
     },
 
     imageClickListener: function() {
       //图片点击监听
       let catPicElement = document.getElementById('catPic');
       catPicElement.addEventListener('click', function() {
-        model.countArr[model.nowClickIndex]++;
-        document.getElementById('clickedCount').innerText = model.countArr[model.nowClickIndex];
+        octopus.setCountArr();
+        document.getElementById('clickedCount').innerText = octopus.getCountArr()[octopus.getNowClickIndex()];
       });
     }
   };
 
+  //连接层
+  let octopus = {
+    init: function() {
+      catListView.init();
+      catView.init();
+    },
+    getCatArr: function() {
+      return model.catArr;
+    },
+
+    getCountArr: function() {
+      return model.countArr;
+    },
+
+    setCountArr: function() {
+      model.countArr[model.nowClickIndex]++;
+    },
+
+    getNowClickIndex: function() {
+      return model.nowClickIndex;
+    },
+
+    setNowClickIndex: function(index) {
+      model.nowClickIndex = index;
+    }
+  };
+
   octopus.init();
-  octopus.listClickListener();
-  octopus.imageClickListener();
 
 };

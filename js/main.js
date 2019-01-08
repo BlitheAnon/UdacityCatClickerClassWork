c63f7bd50e07bc69212b410ff6c7d7a36137322a
blitheanon <blitheanon@qq.com>
2019-01-08 10:49:20 +0800

    Feature: 基本实现CatClicker高级专业版功能

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.onload = function() {
   //模型层
   let model = {
     catArr: ['cat1', 'cat2', 'cat3', 'cat4', 'cat5', 'cat6'],
+    imgArr: ['images/cat1.jpg', 'images/cat2.jpg', 'images/cat3.jpg', 'images/cat4.jpg', 'images/cat5.jpg', 'images/cat6.jpg'],
     countArr: [0, 0, 0, 0, 0, 0],
     nowClickIndex: 0
   }
@@ -23,6 +24,7 @@ window.onload = function() {
 
       this.listClickListener();
     },
+
     listClickListener: function() {
       //列表点击监听
       let list = document.getElementsByTagName('li');
@@ -33,12 +35,17 @@ window.onload = function() {
           return function() {
             //替换猫图，猫名，点击数
             document.getElementById('catName').innerText = catNameCopy;
-            document.getElementById('catPic').src = `images/${catNameCopy}.jpg`;
+            document.getElementById('catPic').src = octopus.getImgArr()[i];
             document.getElementById('clickedCount').innerText = octopus.getCountArr()[i];
             octopus.setNowClickIndex(i);
           };
         })(catName));
       }
+    },
+
+    updateView: function() {
+      //更新列表区域数据
+      this.init();
     }
   };
 
@@ -61,6 +68,13 @@ window.onload = function() {
         octopus.setCountArr();
         document.getElementById('clickedCount').innerText = octopus.getCountArr()[octopus.getNowClickIndex()];
       });
+    },
+
+    updateView: function(name, url, count) {
+      //更新catView区域数据
+      document.getElementById('catName').innerText = name;
+      document.getElementById('catPic').src = url;
+      document.getElementById('clickedCount').innerText = count;
     }
   };
 
@@ -68,21 +82,52 @@ window.onload = function() {
     init: function() {
       this.showAdminArea();
       this.hideAdminArea();
+      this.saveAdminData();
     },
+
     showAdminArea: function() {
       //显示管理员区域
       let adminButton = document.getElementById('adminBtn');
+      let that = this;
       adminButton.addEventListener('click', function() {
         document.getElementById('adminArea').style.display = 'inline';
+        that.initDataToAdmin();
       });
     },
+
     hideAdminArea: function() {
       //隐藏管理员区域
-      let cancelButton=document.getElementById('cancelBtn');
-      cancelButton.addEventListener('click',function() {
+      let cancelButton = document.getElementById('cancelBtn');
+      cancelButton.addEventListener('click', function() {
         document.getElementById('adminArea').style.display = 'none';
       });
-    }
+    },
+
+    initDataToAdmin: function() {
+      //填充数据到管理员区域
+      let nameIpt = document.getElementById('nameIpt');
+      let imgUrlIpt = document.getElementById('imgUrlIpt');
+      let clickCountIpt = document.getElementById('clickCountIpt');
+
+      nameIpt.value = document.getElementById('catName').innerText;
+      imgUrlIpt.value = document.getElementById('catPic').src;
+      clickCountIpt.value = document.getElementById('clickedCount').innerText;
+    },
+    saveAdminData: function() {
+      //保存输入数据
+      let saveButton = document.getElementById('saveBtn');
+      let that = this;
+      saveBtn.addEventListener('click', function() {
+        //取得输入数据
+        let catName = document.getElementById('nameIpt').value;
+        let imgUrl = document.getElementById('imgUrlIpt').value;
+        let clickCount = document.getElementById('clickCountIpt').value;
+        //通知章鱼层更新模型层数据
+        octopus.updateModel(catName, imgUrl, clickCount);
+        //隐藏管理员区域
+        document.getElementById('adminArea').style.display = 'none';
+      });
+    },
   };
 
   //章鱼连接层
@@ -94,6 +139,10 @@ window.onload = function() {
     },
     getCatArr: function() {
       return model.catArr;
+    },
+
+    getImgArr: function() {
+      return model.imgArr;
     },
 
     getCountArr: function() {
@@ -110,6 +159,20 @@ window.onload = function() {
 
     setNowClickIndex: function(index) {
       model.nowClickIndex = index;
+    },
+
+    updateModel: function(name, url, count) {
+      let nowClickIndex = model.nowClickIndex;
+      //给模型层设置新数据
+      model.catArr[nowClickIndex] = name;
+      model.countArr[nowClickIndex] = count;
+      model.imgArr[nowClickIndex]=url;
+      console.log(model.catArr);
+      console.log(model.countArr);
+      //更新catListView
+      catListView.updateView();
+      //更新catView
+      catView.updateView(name, url, count);
     }
   };
 
